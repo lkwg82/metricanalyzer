75df12e3f0c0cdcce389cfd054d4a4c373d4668a
lkwg82@gmx.de <lkwg82@gmx.de@4dd12e21-5482-a3dd-f1a8-39a92c7f625d>
2012-03-11 16:48:05 +0000

    cleanup

--- src/main/java/de/lgohlke/io/JavaSourceFileFinder.java
+++ src/main/java/de/lgohlke/io/JavaSourceFileFinder.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,47 +27,47 @@ import org.apache.commons.io.filefilter.IOFileFilter;
 @RequiredArgsConstructor
 public class JavaSourceFileFinder
 {
-  private Set<IFileVisitor> visitors = new HashSet<IFileVisitor>();
+	private final Set<IFileVisitor> visitors = new HashSet<IFileVisitor>();
 
-  /**
-   * <p>
-   * scan.
-   * </p>
-   */
-  public JavaSourceFileFinder scanDirectory(final File file)
-  {
+	/**
+	 * <p>
+	 * scan.
+	 * </p>
+	 */
+	public JavaSourceFileFinder scanDirectory(final File file)
+	{
 
-    if (file.isDirectory())
-    {
-      IOFileFilter fileFilter = FileFilterUtils.and(//
-          HiddenFileFilter.VISIBLE, //
-          FileFilterUtils.fileFileFilter(),//
-          FileFilterUtils.suffixFileFilter("java", IOCase.INSENSITIVE)//
-          );
-      IOFileFilter directoryFilter = FileFilterUtils.and(HiddenFileFilter.VISIBLE, FileFilterUtils.directoryFileFilter());
-      List<File> files = new ArrayList<File>(FileUtils.listFiles(file, fileFilter, directoryFilter));
+		if (file.isDirectory())
+		{
+			IOFileFilter fileFilter = FileFilterUtils.and(//
+					HiddenFileFilter.VISIBLE, //
+					FileFilterUtils.fileFileFilter(),//
+					FileFilterUtils.suffixFileFilter("java", IOCase.INSENSITIVE)//
+					);
+			IOFileFilter directoryFilter = FileFilterUtils.and(HiddenFileFilter.VISIBLE, FileFilterUtils.directoryFileFilter());
+			List<File> files = new ArrayList<File>(FileUtils.listFiles(file, fileFilter, directoryFilter));
 
-      for (File f : files)
-      {
-        for (IFileVisitor visitor : visitors)
-        {
-          visitor.visit(f);
-        }
-      }
-    }
-    else
-    {
+			for (File f : files)
+			{
+				for (IFileVisitor visitor : visitors)
+				{
+					visitor.visit(f);
+				}
+			}
+		}
+		else
+		{
 
-    }
-    return this;
-  }
+		}
+		return this;
+	}
 
-  public JavaSourceFileFinder addVisitor(final IFileVisitor iFileVisitor)
-  {
-    if (!visitors.add(iFileVisitor))
-    {
-      log.warn("visitor already set : " + iFileVisitor);
-    }
-    return this;
-  }
+	public JavaSourceFileFinder addVisitor(final IFileVisitor iFileVisitor)
+	{
+		if (!visitors.add(iFileVisitor))
+		{
+			log.warn("visitor already set : " + iFileVisitor);
+		}
+		return this;
+	}
 }
