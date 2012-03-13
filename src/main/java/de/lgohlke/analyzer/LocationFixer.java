ba56f021da6dc2f157a38f3cad5c158ab057e912
lkwg82@gmx.de <lkwg82@gmx.de@4dd12e21-5482-a3dd-f1a8-39a92c7f625d>
2012-03-13 18:18:21 +0000

    fixed: removed empty if statements

--- src/main/java/de/lgohlke/analyzer/LocationFixer.java
+++ src/main/java/de/lgohlke/analyzer/LocationFixer.java
@@ -16,48 +16,47 @@ import de.lgohlke.AST.Location;
 @RequiredArgsConstructor
 public class LocationFixer
 {
-  private final SourceCodeSearchEngine searchEngine;
+	private final SourceCodeSearchEngine searchEngine;
 
-  public SourceCode fixLocation(final Location location)
-  {
+	public SourceCode fixLocation(final Location location)
+	{
 
-    SourceCode code = searchEngine.search(location.getKey());
-    if (code == null)
-    {
-      final String key = location.getClazz().replaceAll("\\.", "/");
-      final String method = location.getKey().replaceAll(".*#", "#");
+		SourceCode code = searchEngine.search(location.getKey());
+		if (code == null)
+		{
+			final String key = location.getClazz().replaceAll("\\.", "/");
+			final String method = location.getKey().replaceAll(".*#", "#");
 
-      Collection<SourceCode> codeLists = searchEngine.search(new Query()
-      {
-        @Override
-        public boolean match(final SourceCode unit)
-        {
-          return unit.getKey().startsWith(key) && unit.getKey().endsWith(method);
-        }
-      });
+			Collection<SourceCode> codeLists = searchEngine.search(new Query()
+			{
+				@Override
+				public boolean match(final SourceCode unit)
+				{
+					return unit.getKey().startsWith(key) && unit.getKey().endsWith(method);
+				}
+			});
 
-      if (codeLists.size() == 1)
-      {
-        code = new ArrayList<SourceCode>(codeLists).get(0);
-        if (log.isDebugEnabled())
-        {
-          log.debug("fix location key for " + location.getKey() + " -> " + code.getKey());
-        }
-        location.setKey(code.getKey());
-      }
-      else
-      {
-        log.error("nothing found for key: " + location.getKey());
-      }
-    }
-    else
-    {
-      // if (log.isDebugEnabled())
-      // {
-      // log.debug("did not need to fix  for " + location.getKey());
-      // }
-    }
-    return code;
+			if (codeLists.size() == 1)
+			{
+				code = new ArrayList<SourceCode>(codeLists).get(0);
+				if (log.isDebugEnabled())
+				{
+					log.debug("fix location key for " + location.getKey() + " -> " + code.getKey());
+				}
+				location.setKey(code.getKey());
+			}
+			else
+			{
+				log.error("nothing found for key: " + location.getKey());
+			}
+		}
+		else
+		{
+			if (log.isDebugEnabled()) {
+				log.debug("did not need to fix  for " + location.getKey());
+			}
+		}
+		return code;
 
-  }
+	}
 }
