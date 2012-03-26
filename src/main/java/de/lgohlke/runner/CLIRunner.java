a65dbcec6b8a753c02aae6bb33b09fe353336776
lkwg82@gmx.de <lkwg82@gmx.de@4dd12e21-5482-a3dd-f1a8-39a92c7f625d>
2012-03-26 13:59:52 +0000

    prevent NPE with missing args

--- src/main/java/de/lgohlke/runner/CLIRunner.java
+++ src/main/java/de/lgohlke/runner/CLIRunner.java
@@ -23,120 +23,131 @@ import de.lgohlke.io.bo.TEST_TYPE;
 @Slf4j
 public class CLIRunner
 {
-	private OutputStream out = System.out;
+  private OutputStream out = System.out;
 
-	public static void main(final String[] args)
-	{
-		try
-		{
-			new CLIRunner().parseCLIandCreateInstance(args).analyze();
-		}
-		catch (ConfigurationException e)
-		{
-			// ok
-		}
-		catch (OptionException e)
-		{
-			// ok
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+  public static void main(final String[] args)
+  {
+    try
+    {
+      new CLIRunner().parseCLIandCreateInstance(args).analyze();
+    }
+    catch (ConfigurationException e)
+    {
+      // ok
+    }
+    catch (OptionException e)
+    {
+      // ok
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
 
-	public MetricAnalyzer parseCLIandCreateInstance(final String[] args) throws IOException, ConfigurationException
-	{
-		String[] _args;
-		if (args == null)
-		{
-			_args = new String[] {};
-		}
-		else
-		{
-			_args = args;
-		}
+  public MetricAnalyzer parseCLIandCreateInstance(final String[] args) throws IOException, ConfigurationException
+  {
+    String[] _args;
+    if (args == null)
+    {
+      _args = new String[] {};
+    }
+    else
+    {
+      _args = args;
+    }
 
-		OptionParser parser = new OptionParser();
+    OptionParser parser = new OptionParser();
 
-		ArgumentAcceptingOptionSpec<File> mavenOptSpec = parser
-				.acceptsAll(asList("m", "maven-project-path"), "analyze a maven project and give path to directory the pom.xml sits in").//
-				withRequiredArg().ofType(File.class);
-		ArgumentAcceptingOptionSpec<File> sourceOptSpec = parser.acceptsAll(asList("s", "source-dir"), "directory of sources, could be a tree").withRequiredArg()
-				.ofType(File.class);
-		ArgumentAcceptingOptionSpec<File> outputOptSpec = parser.acceptsAll(asList("o", "output-dir"), "directory for output").withRequiredArg().ofType(File.class);
+    ArgumentAcceptingOptionSpec<File> mavenOptSpec = parser
+        .acceptsAll(asList("m", "maven-project-path"), "analyze a maven project and give path to directory the pom.xml sits in").//
+        withRequiredArg().ofType(File.class);
+    ArgumentAcceptingOptionSpec<File> sourceOptSpec = parser.acceptsAll(asList("s", "source-dir"), "directory of sources, could be a tree").withRequiredArg()
+        .ofType(File.class);
+    ArgumentAcceptingOptionSpec<File> outputOptSpec = parser.acceptsAll(asList("o", "output-dir"), "directory for output").withRequiredArg().ofType(File.class);
 
-		ArgumentAcceptingOptionSpec<String> helpOptSpec = parser.acceptsAll(asList("h", "?", "help"), "show help").withOptionalArg();
-		// ArgumentAcceptingOptionSpec<String> verboseOptSpec = parser.acceptsAll(asList("v", "verbose"),
-		// "be more verbose").withOptionalArg();
+    ArgumentAcceptingOptionSpec<String> helpOptSpec = parser.acceptsAll(asList("h", "?", "help"), "show help").withOptionalArg();
+    // ArgumentAcceptingOptionSpec<String> verboseOptSpec = parser.acceptsAll(asList("v", "verbose"),
+    // "be more verbose").withOptionalArg();
 
-		ArgumentAcceptingOptionSpec<TEST_TYPE> testTypeOptSpec = parser.acceptsAll(asList("t", "test-type"), "type of failed tests to analyze").withRequiredArg()
-				.ofType(TEST_TYPE.class);
+    ArgumentAcceptingOptionSpec<TEST_TYPE> testTypeOptSpec = parser.acceptsAll(asList("t", "test-type"), "type of failed tests to analyze").withRequiredArg()
+        .ofType(TEST_TYPE.class);
 
-		MetricAnalyzer analyzer = null;
-		try
-		{
-			log.debug("arguments : " + StringUtils.join(_args, " "));
-			// System.out.println(_args.length);
-			OptionSet options = parser.parse(_args);
+    MetricAnalyzer analyzer = null;
+    try
+    {
+      /*
+       * prevent for joining with null values
+       */
+      for (int i = 0; i < _args.length; i++)
+      {
+        if (_args[i] == null)
+        {
+          _args[i] = "";
+        }
+      }
 
-			if (options.has(helpOptSpec))
-			{
-				parser.printHelpOn(getOut());
-			}
-			else if (options.has(mavenOptSpec) && options.has(outputOptSpec))
-			{
-				analyzer = new AnalyzerFactory().createInstanceForMavenWithCustomOutput(mavenOptSpec.value(options), outputOptSpec.value(options));
-			}
-			else if (options.has(mavenOptSpec))
-			{
-				analyzer = new AnalyzerFactory().createInstanceForMaven(mavenOptSpec.value(options));
-			}
-			else if (options.has(sourceOptSpec) && options.has(outputOptSpec))
-			{
-				analyzer = new AnalyzerFactory().createInstanceWithCustomSettings(sourceOptSpec.value(options), outputOptSpec.value(options));
-			}
-			else
-			{
-				throw new ConfigurationException("please add at least -m or -s and -o");
-			}
+      log.debug("arguments : " + StringUtils.join(_args, " "));
+      // System.out.println(_args.length);
+      OptionSet options = parser.parse(_args);
 
-			/*
-			 * some optional
-			 */if (analyzer != null)
-			 {
-				 if (options.has(testTypeOptSpec))
-				 {
+      if (options.has(helpOptSpec))
+      {
+        parser.printHelpOn(getOut());
+      }
+      else if (options.has(mavenOptSpec) && options.has(outputOptSpec))
+      {
+        analyzer = new AnalyzerFactory().createInstanceForMavenWithCustomOutput(mavenOptSpec.value(options), outputOptSpec.value(options));
+      }
+      else if (options.has(mavenOptSpec))
+      {
+        analyzer = new AnalyzerFactory().createInstanceForMaven(mavenOptSpec.value(options));
+      }
+      else if (options.has(sourceOptSpec) && options.has(outputOptSpec))
+      {
+        analyzer = new AnalyzerFactory().createInstanceWithCustomSettings(sourceOptSpec.value(options), outputOptSpec.value(options));
+      }
+      else
+      {
+        throw new ConfigurationException("please add at least -m or -s and -o");
+      }
 
-					 TEST_TYPE type = testTypeOptSpec.value(options);
-					 log.info("adding filter for failed tests of type : " + type);
+      /*
+       * some optional
+       */if (analyzer != null)
+       {
+         if (options.has(testTypeOptSpec))
+         {
 
-					 analyzer.filter(new AnalysisTestFilter(type));
-				 }
-			 }
-			 return analyzer;
-		}
-		catch (ConfigurationException e)
-		{
-			out.write((e.getMessage() + "\n").getBytes());
-			parser.printHelpOn(out);
-			throw e;
-		}
-		catch (OptionException e)
-		{
-			out.write((e.getMessage() + "\n").getBytes());
-			parser.printHelpOn(out);
-			throw e;
-		}
-	}
+           TEST_TYPE type = testTypeOptSpec.value(options);
+           log.info("adding filter for failed tests of type : " + type);
 
-	public OutputStream getOut()
-	{
-		return out;
-	}
+           analyzer.filter(new AnalysisTestFilter(type));
+         }
+       }
+       return analyzer;
+    }
+    catch (ConfigurationException e)
+    {
+      out.write((e.getMessage() + "\n").getBytes());
+      parser.printHelpOn(out);
+      throw e;
+    }
+    catch (OptionException e)
+    {
+      out.write((e.getMessage() + "\n").getBytes());
+      parser.printHelpOn(out);
+      throw e;
+    }
+  }
 
-	public void setOut(final OutputStream out)
-	{
-		this.out = out;
-	}
+  public OutputStream getOut()
+  {
+    return out;
+  }
+
+  public void setOut(final OutputStream out)
+  {
+    this.out = out;
+  }
 }
